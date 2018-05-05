f7aec94a843715f989e4acdc82743fb61a190089
rmarro <30510953+rmarro@users.noreply.github.com>
2018-05-05 11:17:03 -0500

    mind-map resources rendering

--- client/src/components/Resources/Resources.js
+++ client/src/components/Resources/Resources.js
@@ -3,13 +3,53 @@ import { Component } from 'react';
 import { render } from 'react-dom';
 import MindMap from 'react-mindmap';
  
+
+const nodes = [
+  {
+    "text": "Resources",
+  },
+  {
+    "text": "Communication",
+    "url": "",
+
+  },
+  {
+    "text": "Structure",
+    "url": "",
+  },
+  {
+    "text": "Sexual Health",
+    "url": "",
+  }
+]
+
+const connections = [
+  {
+    "source": "Resources",
+    "target": "Communication",
+  },
+  {
+    "source": "Resources",
+    "target": "Structure",
+  },
+  {
+    "source": "Resources",
+    "target": "Sexual Health",
+  }
+]
+
+
 class Example extends Component {
   render() {
     return (
-      <MindMap
-        nodes={this.props.nodes}
-        connections={this.props.connections}
-      />
+      <div className="row">
+        <div className="col-md-8 col-md-offset-2">
+          <MindMap
+            nodes={nodes}
+            connections={connections}
+          />
+        </div>
+      </div>
     );
   }
 }
