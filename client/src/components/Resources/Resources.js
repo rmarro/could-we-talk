3d4e67a071575fbc6f7dccf15cd6d1f6b4ad9641
rmarro <30510953+rmarro@users.noreply.github.com>
2018-05-05 23:04:22 -0500

    created resource mindmap

--- client/src/components/Resources/Resources.js
+++ client/src/components/Resources/Resources.js
@@ -6,20 +6,106 @@ import MindMap from 'react-mindmap';
 const nodes = [
   {
     "text": "Resources",
+    "fx": 0,
+    "fy": 0
   },
+
   {
     "text": "Communication",
-    "url": "",
-
+    "fx": -200,
+    "fy": -150
   },
+  {
+    "text": "Frequency",
+    "url": "",
+    "fx": -400,
+    "fy": -300
+  },
+  {
+    "text": "Tone",
+    "url": "",
+    "fx": -200,
+    "fy": -300
+  },
+  {
+    "text": "Conflict Resolution",
+    "url": "",
+    "fx": -500,
+    "fy": -150
+  },
+
   {
     "text": "Structure",
-    "url": "",
+    "fx": 200,
+    "fy": -150
   },
   {
-    "text": "Sexual Health",
+    "text": "Length",
     "url": "",
-  }
+    "fx": 400,
+    "fy": -300
+  },
+  {
+    "text": "Exclusivity",
+    "url": "",
+    "fx": 200,
+    "fy": -300
+  },
+  {
+    "text": "Purpose",
+    "url": "",
+    "fx": 500,
+    "fy": -150
+  },
+
+  {
+    "text": "Sexual Health",
+    "fx": -200,
+    "fy": 150
+  },
+  {
+    "text": "Sexual Satisfaction",
+    "url": "",
+    "fx": -400,
+    "fy": 300
+  },
+  {
+    "text": "Protection",
+    "url": "",
+    "fx": -200,
+    "fy": 300
+  },
+  {
+    "text": "X1",
+    "url": "",
+    "fx": -500,
+    "fy": 150
+  },
+
+  {
+    "text": "Responsibilities",
+    "fx": 200,
+    "fy": 150
+  },
+  {
+    "text": "Finances",
+    "url": "",
+    "fx": 400,
+    "fy": 300
+  },
+  {
+    "text": "Chores",
+    "url": "",
+    "fx": 200,
+    "fy": 300
+  },
+  {
+    "text": "X2",
+    "url": "",
+    "fx": 500,
+    "fy": 150
+  },
+  
 ]
 
 const connections = [
@@ -34,6 +120,58 @@ const connections = [
   {
     "source": "Resources",
     "target": "Sexual Health",
+  },
+  {
+    "source": "Resources",
+    "target": "Responsibilities",
+  },
+  {
+    "source": "Communication",
+    "target": "Frequency",
+  },
+  {
+    "source": "Communication",
+    "target": "Tone",
+  },
+  {
+    "source": "Communication",
+    "target": "Conflict Resolution",
+  },
+  {
+    "source": "Structure",
+    "target": "Exclusivity",
+  },
+  {
+    "source": "Structure",
+    "target": "Length",
+  },
+  {
+    "source": "Structure",
+    "target": "Purpose",
+  },
+  {
+    "source": "Sexual Health",
+    "target": "Sexual Satisfaction",
+  },
+  {
+    "source": "Sexual Health",
+    "target": "Protection",
+  },
+  {
+    "source": "Sexual Health",
+    "target": "X1",
+  },
+  {
+    "source": "Responsibilities",
+    "target": "Finances",
+  },
+  {
+    "source": "Responsibilities",
+    "target": "Chores",
+  },
+  {
+    "source": "Responsibilities",
+    "target": "X2",
   }
 ]
 
